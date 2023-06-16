d83e5285e8cfec23f7a4d1d36bec541f55d7090b
Vaishnavi493 <115732502+Vaishnavi493@users.noreply.github.com>
2023-06-16 13:29:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const signupForm = document.getElementById('signupForm');
+  
+    loginForm.addEventListener('submit', function(e) }
